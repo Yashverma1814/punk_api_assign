d1088e2a8f279c889fcaabb6249305a6c583ab37
Yash Verma <yashv1752@gmail.com>
2023-08-23 21:55:08 +0530

    card component and home page created

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CardComponent } from '../component/CardComponent'
+
+export const HomePage = () => {
+  return (
+    <div>
+        <CardComponent />
+    </div>
+  )
+}
